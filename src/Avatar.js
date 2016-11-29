ab0dccf224b083522b801de9e60736acc47655c0
Sid <sid26@ucla.edu>
2016-11-29 09:45:00 +0530

    Remove avatar markup

--- src/Avatar.js
+++ src/Avatar.js
@@ -24,11 +24,12 @@ export default class Avatar extends React.Component {
   render() {
     if (this.props.isSameUser(this.props.currentMessage, this.props.nextMessage) && this.props.isSameDay(this.props.currentMessage, this.props.nextMessage)) {
       return (
-        <View style={[styles[this.props.position].container, this.props.containerStyle[this.props.position]]}>
-          <GiftedAvatar
-            avatarStyle={StyleSheet.flatten([styles[this.props.position].image, this.props.imageStyle[this.props.position]])}
-          />
-        </View>
+        // <View style={[styles[this.props.position].container, this.props.containerStyle[this.props.position]]}>
+        //   <GiftedAvatar
+        //     avatarStyle={StyleSheet.flatten([styles[this.props.position].image, this.props.imageStyle[this.props.position]])}
+        //   />
+        // </View>
+        null
       );
     }
     return (
